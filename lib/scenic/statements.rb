d3264b890c58bf3b2063c2b23e07ef0b585ed5b3
Paul Carey <paul@sourcelab.technology>
2016-09-15 16:43:16 +0100

    fix and test statement includes

--- lib/scenic/statements.rb
+++ lib/scenic/statements.rb
@@ -4,6 +4,7 @@ require 'scenic/statements/function_statements'
 module Scenic
   # Methods that are made available in migrations for managing Scenic views.
   module Statements
-    extend ViewStatements
+    include ViewStatements
+    include FunctionStatements
   end
 end
---
+++ spec/scenic/statements_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+module Scenic
+  describe Scenic::Statements do
+
+    subject { Class.new { extend Statements } }
+
+    it { is_expected.to respond_to(:create_view) }
+    it { is_expected.to respond_to(:drop_view) }
+    it { is_expected.to respond_to(:update_view) }
+    it { is_expected.to respond_to(:replace_view) }
+    it { is_expected.to respond_to(:replace_view) }
+
+    it { is_expected.to respond_to(:create_function) }
+    it { is_expected.to respond_to(:drop_function) }
+    it { is_expected.to respond_to(:update_function) }
+    it { is_expected.to respond_to(:replace_function) }
+
+  end
+end
